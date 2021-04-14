bf070f66ac4f2d2aa23564b423acc3904309a6d8
Dai Rees <dai@dai.me>
2021-04-14 00:57:37 -0700

    Making `Filter` more permissive as real validation is done by the actual COM ShellDialog classes - we shouldn't be more restrictive than they are.

--- ShellFileDialogs/Filter.cs
+++ ShellFileDialogs/Filter.cs
@@ -10,31 +10,40 @@ namespace ShellFileDialogs
 		private static readonly Char[] _semiColon = new Char[] { ';' };
 		private static readonly Char[] _pipe      = new Char[] { '|' };
 
-		private static readonly Char[] _extensionTrimStart = new Char[] { ' ', '.', ';' };
-		private static readonly Char[] _extensionTrim      = new Char[] { ' ', '.', ';', '*', '\\', '/', '?' };
-
-		public Filter(String displayName, params String[] extensions)
+		/// <summary></summary>
+		/// <param name="displayName">Required. Cannot be <see langword="null"/>, empty, nor whitespace.</param>
+		/// <param name="extensions">Required. Cannot be <see langword="null"/> or empty (i.e. at least one extension filter must be specified).</param>
+		public Filter( String displayName, params String[] extensions )
 			: this( displayName, (IEnumerable<String>)extensions )
 		{
 		}
 
-		public Filter(String displayName, IEnumerable<String> extensions)
+		/// <summary></summary>
+		/// <param name="displayName">Required. Cannot be <see langword="null"/>, empty, nor whitespace.</param>
+		/// <param name="extensions">Required. The collection cannot be <see langword="null"/> or empty - and it cannot contain null, empty, or whitespace values - nor duplicate values.</param>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="displayName"/> or <paramref name="extensions"/> is <see langword="null"/>.</exception>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="extensions"/> is empty or contains only empty file extensions.</exception>
+		public Filter( String displayName, IEnumerable<String> extensions )
 		{
 			if( String.IsNullOrWhiteSpace(displayName) ) throw new ArgumentNullException(nameof(displayName));
-			if( extensions == null ) throw new ArgumentNullException(nameof(extensions));
+			if( extensions is null ) throw new ArgumentNullException(nameof(extensions));
 
 			this.DisplayName = displayName.Trim();
-			this.Extensions  = extensions
+
+			this.Extensions = extensions
 				.Select( s => s.Trim() ) // Trim whitespace
-				.Select( s => // Trim leading
-					s.StartsWith( "*.", StringComparison.OrdinalIgnoreCase ) ? s.Substring(2) :
-					s.StartsWith( "." , StringComparison.OrdinalIgnoreCase ) ? s.Substring(1) : s
-				)
 				.Where( s => !String.IsNullOrWhiteSpace( s ) )
 				.ToList(); // make a copy to prevent possible changes
+
+			if( this.Extensions.Count == 0 )
+			{
+				throw new ArgumentException( message: "Extensions collection must not be empty, nor can it contain only null, empty or whitespace extensions.", paramName: nameof(extensions));
+			}
 		}
 
 		public String DisplayName { get; }
+
+		/// <summary>All extension values have their leading dot and any leading asterisk filter trimmed, so if &quot;<c>*.wav</c>&quot; is passed as an extension string to <see cref="Filter.Filter(string, string[])"/> then it will appear in this list as &quot;<c>wav</c>&quot;.</summary>
 		public IReadOnlyList<String> Extensions { get; }
 
 		/// <summary>Returns <see langword="null"/> if the string couldn't be parsed.</summary>
